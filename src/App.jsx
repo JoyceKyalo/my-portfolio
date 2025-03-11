07d20114835342622f14026a08e466c3a1855c63
Joyce Kyalo <kyalojoyce0@gmail.com>
2025-03-11 01:32:57 -0500

    added contact.jsx section and its styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from './components/About';
 import Projects from "./components/Projects";
 import Skills from './components/Skills';
-import About from './components/About';
+import Contact from './components/Contact';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -18,6 +19,7 @@ const App = () => {
       <About />
       <Projects />
       <Skills />
+      <Contact />
       
       {/* Other sections will go here */}
     </div>
