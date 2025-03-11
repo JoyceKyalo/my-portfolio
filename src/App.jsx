34c2c20e069bdc53b8df3955458929c568b83e4f
Joyce Kyalo <kyalojoyce0@gmail.com>
2025-03-11 01:27:48 -0500

    added about.jsx section and its styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from './components/Skills';
+import About from './components/About';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -14,8 +15,10 @@ const App = () => {
     <div>
       <Header />
       <Hero />
+      <About />
       <Projects />
       <Skills />
+      
       {/* Other sections will go here */}
     </div>
   );
