34f7acbfa6ffc1b62196c1caffa5f5df4c39d2e4
Joyce Kyalo <joycekyalo@JK-2.local>
2025-03-11 01:41:32 -0500

    added footer.jsx section and its styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 Joyce (JK) Kyalo | All Rights Reserved</p>
+      <div className="social-links">
+        <a href="https://github.com/JoyceKyalo" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/joyce-kyalo-7b47051b0/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="mailto:kyalojoyce0@gmail.com">Email</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
